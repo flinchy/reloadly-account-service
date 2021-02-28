b02cd5474d6f5baf80c35741c6df3ca28e5e32df
chisom <iwowochisom@gmail.com>
2021-02-28 15:35:46 +0100

    add account service constants

---
+++ src/main/java/com/chisom/accountservice/constants/AccountServiceConstants.java
@@ -0,0 +1,15 @@
+package com.chisom.accountservice.constants;
+
+public final class AccountServiceConstants {
+
+    private AccountServiceConstants() {
+    }
+
+    public static final String ACCOUNT_CREATION_SUCCESS_MSG = "Your Savings account has been created successfully and your new account number is : ";
+    public static final String ACCOUNT_CREATION_SUBJECT = "Savings Account";
+    public static final String DEPOSIT_NOTIFICATION_SUBJECT = "Deposit";
+    public static final String WITHDRAW_NOTIFICATION_SUBJECT = "Withdrawal";
+    public static final String NOTIFICATION_MESSAGE = "Transaction was successful";
+    public static final String NOTIFICATION_ERROR_SUBJECT = "Service is Unavailable";
+    public static final String NOTIFICATION_ERROR_MSG = "Please try again later";
+}
