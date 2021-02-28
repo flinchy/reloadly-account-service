f6bd554c3749058875e2d3f8ed6dc8ed42f9d9b7
chisom <iwowochisom@gmail.com>
2021-02-28 16:22:11 +0100

    add account controller

---
+++ src/main/java/com/chisom/accountservice/controller/AccountController.java
@@ -0,0 +1,80 @@
+package com.chisom.accountservice.controller;
+
+import com.chisom.accountservice.dto.request.AccountRegistrationRequest;
+import com.chisom.accountservice.dto.request.LoginRequest;
+import com.chisom.accountservice.dto.request.UpdateAccountRequest;
+import com.chisom.accountservice.dto.response.AccountRegisterResponse;
+import com.chisom.accountservice.dto.response.LoginResponse;
+import com.chisom.accountservice.dto.response.TransactionResponse;
+import com.chisom.accountservice.dto.response.UpdateAccountResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.security.Principal;
+
+/**
+ * @author Chisom.Iwowo
+ */
+@RestController
+@RequestMapping("/account")
+public interface AccountController {
+
+    /**
+     * endpoint to register a new account
+     *
+     * @param accountRegistrationRequest registration request
+     * @return AccountRegisterResponse
+     */
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    AccountRegisterResponse register(
+            @Valid @RequestBody final AccountRegistrationRequest accountRegistrationRequest);
+
+    /**
+     * login endpoint
+     *
+     * @param loginRequest login request
+     * @return LoginResponse
+     */
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    LoginResponse login(@Valid @RequestBody LoginRequest loginRequest);
+
+    /**
+     * endpoint to deposit money into savings account
+     *
+     * @param amount    amount
+     * @param principal logged in user
+     * @return String
+     */
+    @PostMapping("/deposit")
+    @ResponseStatus(HttpStatus.OK)
+    TransactionResponse deposit(
+            @RequestParam final double amount, Principal principal, HttpServletRequest request);
+
+    /**
+     * endpoint to withdraw money.
+     *
+     * @param amount    amount
+     * @param principal logged in user
+     * @return String
+     */
+    @PostMapping("withdraw")
+    @ResponseStatus(HttpStatus.OK)
+    TransactionResponse withdraw(
+            @RequestParam final double amount, Principal principal, HttpServletRequest request);
+
+    /**
+     * update account details
+     *
+     * @param updateAccountRequest update request
+     * @return Object
+     */
+    @PostMapping("update")
+    @ResponseStatus(HttpStatus.CREATED)
+    UpdateAccountResponse updateSavingsAccount(
+            @Valid @RequestBody final UpdateAccountRequest updateAccountRequest, Principal principal);
+}
+
