e22bcac56eb36a9a63abf9e075bc5c8c4c66ddac
chisom <iwowochisom@gmail.com>
2021-02-28 15:33:27 +0100

    add app constants

---
+++ src/main/java/com/chisom/accountservice/utils/AppConstants.java
@@ -0,0 +1,11 @@
+package com.chisom.accountservice.utils;
+
+public final class AppConstants {
+
+    private AppConstants() {
+    }
+
+    public static final String DEPOSIT_NARRATION ="Deposit to your account was successful";
+    public static final String WITHDRAWAL_NARRATION="Withdrawal was successful";
+    public static final String INITIAL_SAVINGS_ACCOUNT_BALANCE = "0.0";
+}
