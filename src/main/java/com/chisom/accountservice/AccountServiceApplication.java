12ebb1962a51a6211f5bea8e28df2c159cf046f6
chisom <iwowochisom@gmail.com>
2021-03-01 02:59:27 +0100

    enable swagger

--- src/main/java/com/chisom/accountservice/AccountServiceApplication.java
+++ src/main/java/com/chisom/accountservice/AccountServiceApplication.java
@@ -3,9 +3,11 @@ package com.chisom.accountservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableSwagger2
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
