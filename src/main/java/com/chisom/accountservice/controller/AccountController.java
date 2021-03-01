3cb35447eab23fe62a7842f83f54fba109a9653b
chisom <iwowochisom@gmail.com>
2021-03-01 03:18:01 +0100

    modify account controller

--- src/main/java/com/chisom/accountservice/controller/AccountController.java
+++ src/main/java/com/chisom/accountservice/controller/AccountController.java
@@ -7,8 +7,6 @@ import com.chisom.accountservice.dto.response.AccountRegisterResponse;
 import com.chisom.accountservice.dto.response.LoginResponse;
 import com.chisom.accountservice.dto.response.TransactionResponse;
 import com.chisom.accountservice.dto.response.UpdateAccountResponse;
-import io.swagger.annotations.ApiModelProperty;
-import io.swagger.annotations.ApiParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
